520a7e6386516bf8dee235fbb9ae495a98c7551c
Kyle Morton <mkmorton91@gmail.com>
2025-08-01 20:33:10 -0500

    adding basic binding with isNight

--- SwiftUI-Weather/BackgroundView.swift
+++ SwiftUI-Weather/BackgroundView.swift
@@ -8,18 +8,3 @@
 import SwiftUI
 
 
-struct BackgroundView: View {
-    
-    var topColor: Color
-    var bottomColor: Color
-    
-    var body: some View {
-        
-        // You can add as many colors to the array (it's really cool)
-        LinearGradient(
-            colors: [topColor, bottomColor],
-            startPoint: .topLeading,
-            endPoint: .bottomTrailing)
-        .edgesIgnoringSafeArea(.all)
-    }
-}
--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -5,21 +5,38 @@
 //  Created by Kyle Morton on 7/30/25.
 //
 
+/*
+ 
+ CHALLENGES
+ 1) Create a data model of the weather day (dayOfTheWeek, imageName, temp)
+ 2) Find a basic weather API and pull the weather data dynamically
+ 3) Tab view different cities? 
+ */
+
+
+
 import SwiftUI
 
 struct ContentView: View {
+    
+    // Structs don't hold state (views get created and destroyed all the time)
+    // @State let's swift know to keep track of this value even if the view itself is recycled
+    @State private var isNight:Bool = false
+    
     var body: some View {
         ZStack {
             
             BackgroundView(
-                topColor: Color.blue,
-                bottomColor: Color("LightBlue")
+                isNight: $isNight
             )
             
             VStack {
                 CityTextView(cityName: "Cupertino, CA")
                 
-                MainWeatherStatusView(imageName: "cloud.sun.fill", temperature: 78)
+                MainWeatherStatusView(
+                    isNight: $isNight,
+                    temperature: 78
+                )
                     
                 HStack(spacing:20) {
                     WeatherDayView(dayOfTheWeek: "TUE", imageName: "cloud.sun.fill", temperature: 78)
@@ -30,8 +47,19 @@ struct ContentView: View {
                 }
                 
                 Spacer()
-            
-                WeatherButton(title: "Change Day Time", textColor: .blue, backgroundColor: .white)
+//            
+//                WeatherButton(title: "Change Day Time", textColor: .blue, backgroundColor: .white)
+//                
+                Button {
+                    isNight.toggle()
+                } label: {
+                    Text("Change Day Time")
+                        .frame(width: 280, height: 50)
+                        .background(.white)
+                        .foregroundColor(Color.blue)
+                        .font(.system(size: 20, weight: .bold))
+                        .cornerRadius(10)
+                }
                 
                 Spacer()
             }
@@ -41,6 +69,24 @@ struct ContentView: View {
 
 // If it's unique to the current view, leave the structs in this same file
 // if something global, put in it's own file
+
+struct BackgroundView: View {
+    
+    @Binding var isNight: Bool
+
+    var body: some View {
+        
+        // You can add as many colors to the array (it's really cool)
+        LinearGradient(
+            colors: [isNight
+                     ? Color.black : Color.blue,
+                     isNight
+                     ? Color.gray : Color("LightBlue")],
+            startPoint: .topLeading,
+            endPoint: .bottomTrailing)
+        .edgesIgnoringSafeArea(.all)
+    }
+}
 
 
 struct CityTextView: View {
@@ -54,12 +100,12 @@ struct CityTextView: View {
 }
 
 struct MainWeatherStatusView: View {
-    var imageName: String
+    @Binding var isNight: Bool
     var temperature: Int
     
     var body: some View {
         VStack (spacing: 10) {
-            Image(systemName: imageName)
+            Image(systemName: isNight ? "moon.stars.fill" : "cloud.sun.fill")
                 .renderingMode(.original) // converts it from black to a color icon
                 .resizable()
                 .aspectRatio(contentMode: .fit ) // forces the image to fit it's aspect ratio into the container (our frame below)
